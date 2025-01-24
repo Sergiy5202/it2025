60b91d78bdf4deeb79ae252e411713cd0c2a8f99
Sergiy11 <sergiy111@ukr.net>
2025-01-24 12:50:13 +0200

    24.01.25

--- vykonS/query1.sql
+++ vykonS/query1.sql
@@ -1,25 +1,22 @@
--- Active: 1702282272087@@127.0.0.1@5432@it2025practice13
+-- Active: 1737624182324@@127.0.0.1@5432@militarytrainingdb@public
 
 CREATE DATABASE MilitaryTrainingDB;
 
 -- Створення таблиці
 CREATE TABLE TrainingPrograms (
     id SERIAL PRIMARY KEY,
-    ProgramID (50) NOT NULL,
-    Title (50) NOT NULL,
-    Duration (50) NOT NULL,
-    Participants (100) NOT NULL,
-    CompletionDate (100) NOT NULL
+    ProgramID VARCHAR(50) NOT NULL,
+    Title VARCHAR(50) NOT NULL,
+    Duration VARCHAR(50) NOT NULL,
+    Participants VARCHAR(100) NOT NULL,
+    CompletionDate VARCHAR(100) NOT NULL
 );
 
 -- Вставка даних
 INSERT INTO TrainingPrograms (ProgramID, Title, Duration, Participants,CompletionDate)
 VALUES
-('ProgramID`, `Title`, `Duration`, `Participants`, `CompletionDate),
-('Olena', 'Kovalenko', 'Lieutenant', 'Communications Officer'),
-('Andrii', 'Shevchenko', 'Major', 'Battalion Commander'),
-('Oksana', 'Ivchenko', 'Sergeant', 'Logistics Specialist'),
-('Mykola', 'Bondarenko', 'Colonel', 'Regimental Commander');
+(1, 'Example Program', '3 months', 25, '2025-01-24'),
+(2, 'Another Program', '6 months', 50, '2025-06-15');
 
 -- Перевірка даних
-SELECT * FROM personal;
+SELECT * FROM TrainingPrograms;
